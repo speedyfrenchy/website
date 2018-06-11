54852a2b131dc89867da088167915b8806d4697c
Daniel <daniel@tangi0.com>
2018-06-11 13:10:30 +0100

    Allow for actually secret admin password

--- blog/config.py
+++ blog/config.py
@@ -2,10 +2,11 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'definitely-a-secret'
 
-    ADMIN_PASSWORD = 'secret'
+    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD') or 'secret'
 
     DATABASE = 'sqliteext:///%s' % os.path.join(basedir, 'blog.db')
 
