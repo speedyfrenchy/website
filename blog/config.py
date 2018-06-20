e40c8ba8d999f7cc79f63b8738803d9244ef3b5f
Daniel <daniel@tangi0.com>
2018-06-20 13:50:42 +0100

    remove secrets from config file

--- blog/config.py
+++ blog/config.py
@@ -4,9 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'definitely-a-secret'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD') or 'secret'
+    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
 
     DATABASE = 'sqliteext:///%s' % os.path.join(basedir, 'blog.db')
 
